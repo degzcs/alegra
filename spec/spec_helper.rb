0bea845f7b1024d88766d196063189dec184e2a9
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-10-12 18:00:34 -0500

    add API responses for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require 'alegra'
 Dir[File.join('./lib/alegra/**/*.rb')].each { |f| require f }
+Dir[File.join('./spec/support/**/*.rb')].each { |f| require f }
 require 'rspec'
 require 'pry'
 
@@ -8,6 +9,10 @@ require 'webmock/rspec'
 require 'vcr'
 
 VCR.configure do |c|
-  c.cassette_library_dir = "spec/fixtures"
+  c.cassette_library_dir = "spec/fixtures/cassettes"
   c.hook_into :webmock
+end
+
+RSpec.configure do |config|
+  config.include JsonReponses
 end
